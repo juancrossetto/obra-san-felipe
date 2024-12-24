ab0e3131c676bcc7bfe93dad3ecdc12b0cf94ff2
Juanma Crossetto <juancrossetto@gmail.com>
2024-12-24 16:28:00 -0300

    feat: change in s3 route

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,26 +1,34 @@
 import { NextResponse } from 'next/server';
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { addInstallment, updateInstallment, deleteInstallment } from '@/lib/installments'; // Usa la lógica centralizada
-import { getSheetsData } from '@/lib/googleSheets';
+// import { addInstallment, updateInstallment, deleteInstallment } from '@/lib/installments'; // Usa la lógica centralizada
+// import { getSheetsData } from '@/lib/googleSheets';
 
 const s3Client = new S3Client({
-    region: process.env.NEXT_PUBLIC_AWS_S3_REGION || "",
+    region: process.env.NEXT_PUBLIC_AWS_S3_REGION || "eu-west-2",
     credentials: {
         accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID || "",
         secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY || "",
     },
 })
+
+async function uploadFileToS3(file: any, fileName: string) {
+
+	const fileBuffer = file;
+	console.log(fileName);
+
+	const params = {
+		Bucket: process.env.AWS_S3_BUCKET_NAME,
+		Key: `${fileName}`,
+		Body: fileBuffer,
+		ContentType: "image/jpg"
+	}
+
+	const command = new PutObjectCommand(params);
+	await s3Client.send(command);
+	return fileName;
+}
 export async function GET(req: Request) {
     try {
-        // const rowInstallments = await getSheetsData(INSTALLMENTS_TAB_ID);
-        // const formattedInstallments = rowInstallments.map((installment: any) => ({
-        //   date: installment["Mes"],
-        //   installment: installment["Cuotas"],
-        //   amount: parseFloat(installment["Monto"].replace(/\./g, '').replace(/,/g, '.')),
-        //   dueDate: installment["Fecha Pago"],
-        //   isPaid: installment["Fecha Pago"] ? true : false,
-        // }))
-
         return NextResponse.json({ msg: "Hello API!" })
     } catch (error) {
         console.error('Error fetching installments:', error);
@@ -28,11 +36,21 @@ export async function GET(req: Request) {
     }
 }
 
-export async function POST(req: Request) {
-    try {
+export async function POST(request: Request) {
+	try {
 
-        return NextResponse.json({ msg: "Hello API!" })
-    } catch (error) {
-        return NextResponse.json({ error: 'Error uploading file' });
-    }
+		const formData = await request.formData();
+		const file: any = formData.get("file");
+
+		if(!file) {
+			return NextResponse.json( { error: "File is required."}, { status: 400 } );
+		} 
+
+		const buffer = Buffer.from(await file.arrayBuffer());
+		const fileName = await uploadFileToS3(buffer, file.name);
+
+		return NextResponse.json({ success: true, fileName});
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
 }
